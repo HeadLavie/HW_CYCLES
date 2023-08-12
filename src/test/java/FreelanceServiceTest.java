898df39f322b9b8b47364340ec4dff5b5589f77e
lavie head <nadya.karpenko@gmail.com>
2023-08-12 13:17:47 +0100

    Second Task

--- src/test/java/FreelanceServiceTest.java
+++ src/test/java/FreelanceServiceTest.java
@@ -1,17 +1,27 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import ru.netology.services.FreelanceService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import ru.netology.services.FreelanceService;
 
 public class FreelanceServiceTest {
 
-    @Test
-    public void shouldCalcCount() {
+    @ParameterizedTest
+
+ // @CsvSource({
+//    "3, 10000, 3000, 20000",
+//    "2, 100000, 60000, 150000"
+//   })
+
+    @CsvFileSource(files = "src\test\resources\freelance.csv")
+    public void testCalcCount(int expected, int income, int expenses, int threshhold) {
         FreelanceService service = new FreelanceService();
 
-        int expected = 2;
-        int actual = service.calcFreelance(100000, 60000, 150000);
+        //int expected = 2;
+        int actual = service.calcFreelance(income, expenses, threshhold);
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expected,actual);
     }
 
 }
